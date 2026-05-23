6752be58503119ad5cfe5ddd7e433f2331049de9
Harshit Nigam <harshitnigam085@gmail.com>
2026-05-23 12:04:06 +0530

    push

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use(express.json());
 
 // DB Connection
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST || "localhost",
-  port: process.env.MYSQLPORT || 3306,
-  user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "",
-  database: process.env.MYSQLDATABASE || "msvm_school",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "msvm_school",
   ssl: {
     rejectUnauthorized: false
   }
